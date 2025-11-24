c802471281dd3208daef09b4c21e6b8a88ed4c91
andresortegac <andres.ortegac97@gmail.com>
2025-11-23 20:34:35 -0500

    MVC Login

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::controller(LoginController::class)->group(function(){
+    Route::get('login', 'show');
 });
