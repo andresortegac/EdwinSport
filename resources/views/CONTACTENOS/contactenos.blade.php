bf8c588688003204af6f276df0d0a293c07a9cb1
andresortegac <andres.ortegac97@gmail.com>
2025-11-25 19:48:48 -0500

    vista contactenos

---
+++ resources/views/CONTACTENOS/contactenos.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <link rel="stylesheet" href="{{ asset('/CSS/contactenos.css') }}">
+
+        <title>Crear un evento</title>
+
+    </head>
+
+    <body>
+
+        <h1>Reservar una cita</h1>
+
+        <div class="container">
+
+            <label>Name</label>
+            <input type="text" placeholder="Enter your Name">
+
+            <label>Phone</label>
+            <input type="text" placeholder="Enter your phone (e.g. +14155552675)">
+
+            <label>Message</label>
+            <textarea rows="5" placeholder="Enter your message"></textarea>
+
+            <button>ENVIAR</button>
+        </div>
+
+    </body>
+</html>
+
